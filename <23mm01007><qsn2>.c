6607ad7b6b9551a9794adabcd7b2332f05172262
CVedaReddy <157104274+CVedaReddy@users.noreply.github.com>
2024-04-05 15:13:12 +0530

    Update <23mm01007><qsn2>.c

--- <23mm01007><qsn2>.c
+++ <23mm01007><qsn2>.c
@@ -1,1 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[1000], stack[1000];
+    int top = -1;
+    printf("Input a string: ");
+    scanf("%s", s);
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (top == -1 || stack[top] != s[i])
+            stack[++top] = s[i];
+        else
+            top--;
+    }
+    stack[++top] = '\0';
+    printf("output: %s\n", stack);
+    return 0;
+}
